5c2b3da5229a001e2f0d41c6c5a01df4902657eb
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-02 02:40:22 +0000

    Caesar cipher
    incrementLetter needs work (end of workshop)

--- caesar.c.c
+++ caesar.c.c
@@ -1,50 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-void rotate(char *letter); 
-void rotate2(char *letter, int key);
+void rotate(char *letter, int key);
 void encoder(char *inputText, int key);
+void incrementLetter(char *inputText, int key);
 
 int main(){   
-    //char inputText[100] = "zzz";
-    int key = 3;        //hardcoded atm
-    char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char inputText[100] = "hello";
+    int key = 5;        //hardcoded atm
+    //char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i;
         
-    rotate2(letter, key);       //code to test rotation for decoding
+            
+    /*rotate(letter, key);       //code to test rotation for decoding
     for (i = 0; i <= 26; i++)
-        printf("%c ", letter[i]);
+        printf("%c ", letter[i]); */
     
-    /*encoder(inputText, key);
+    incrementLetter(inputText, key);
     for (i = 0; i <= 26; i++)
-        printf("%c ", inputText[i]); */
+        printf("%c ", inputText[i]);
     
     return 0;
 }
 
 
-
-void encoder(char *inputText, int key){
-    int n = 0;
-    while((inputText[n] >= 'A') && (inputText[n] <= 'z')){
-        inputText[n] = inputText[n] + key;
-        n++;
+void incrementLetter(char *inputText, int key){
+    int n;
+    for(n = 0; inputText[n] == 0; n++){
+        inputText[n] = inputText[n] - 1 - key;
+        if(inputText[n] < 'A'){
+           inputText[n] = inputText[n] + 26;
+        }
     }
 }
+
+
+
+
+
+
+
+
 
 
 
 
 //--------------------------------------------------------------------------//
-void rotate(char *letter){
-    int n;
-    static int c = 0;
-    for(n = 0; n <= (25 - c); n++){
-        letter[n] = letter[n - 1 + c];
+
+void encoder(char *inputText, int key){
+    int n = 0;
+    while((inputText[n] >= 'A') && (inputText[n] <= 'z')){
+        inputText[n] = inputText[n] - 1 - key;
+        n++;
     }
-    c++;
 }
 
-void rotate2(char *letter, int key){
+//--------------------------------------------------------------------------//
+
+void rotate(char *letter, int key){
     int n;
     for(n = 26; n >= 0; n--){
        letter [n] = letter[n] - 1 - key; 
@@ -52,7 +64,6 @@ void rotate2(char *letter, int key){
        if(letter[n] < 'A'){
            letter[n] = letter[n] + 26;
        }
-       
     }
 }
 
