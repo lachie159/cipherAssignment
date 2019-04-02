9fd71b8959afd089feffaadc1e0056f49cb4b73d
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-02 02:26:38 +0000

    Caesar cipher
    Working rotation

--- caesar.c.c
+++ caesar.c.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 void rotate(char *letter); 
-void rotate2(char *letter);
+void rotate2(char *letter, int key);
 void encoder(char *inputText, int key);
-void decoderKey(char *inputText, )
 
 int main(){   
-    char inputText[100] = "hello";
+    //char inputText[100] = "zzz";
     int key = 3;        //hardcoded atm
-    //char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i;
         
-    /*rotate(letter);       //code to test rotation for decoding
-    rotate2(letter);
+    rotate2(letter, key);       //code to test rotation for decoding
     for (i = 0; i <= 26; i++)
-        printf("%c ", letter[i]); */
+        printf("%c ", letter[i]);
     
-    encoder(inputText, key);
+    /*encoder(inputText, key);
     for (i = 0; i <= 26; i++)
-        printf("%c ", inputText[i]);
+        printf("%c ", inputText[i]); */
     
     return 0;
 }
+
+
+
 void encoder(char *inputText, int key){
     int n = 0;
     while((inputText[n] >= 'A') && (inputText[n] <= 'z')){
@@ -33,36 +34,31 @@ void encoder(char *inputText, int key){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //--------------------------------------------------------------------------//
 void rotate(char *letter){
     int n;
     static int c = 0;
     for(n = 0; n <= (25 - c); n++){
-        letter[n] = letter[n + 1 + c];
+        letter[n] = letter[n - 1 + c];
     }
     c++;
 }
 
-void rotate2(char *letter){
+void rotate2(char *letter, int key){
     int n;
-    int d = 0;
-    static int c = 0;
-    for(n = 26; n >= (26 - c); n--){
-        letter[n] = letter['A' + c + d];
-        d--;
+    for(n = 26; n >= 0; n--){
+       letter [n] = letter[n] - 1 - key; 
+       
+       if(letter[n] < 'A'){
+           letter[n] = letter[n] + 26;
+       }
+       
     }
 }
+
+
+
+
+
+
+
