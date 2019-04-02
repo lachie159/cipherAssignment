e16b73622584a21ed10147498fd106e9b3813db7
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-02 01:52:49 +0000

    Caesar cipher
    Succesfully encodes a message up to 100 characters

--- caesar.c.c
+++ caesar.c.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-void rotate(char *letter);
+void rotate(char *letter); 
 void rotate2(char *letter);
+void encoder(char *inputText, int key);
 
 int main(){   
-    char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char inputText[100] = "hello";
+    int key = 3;        //hardcoded atm
+    //char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i;
-    
-    rotate(letter);
+        
+    /*rotate(letter);       //code to test rotation for decoding
     rotate2(letter);
-    
     for (i = 0; i <= 26; i++)
-        printf("%c ", letter[i]);
+        printf("%c ", letter[i]); */
+    
+    encoder(inputText, key);
+    for (i = 0; i <= 26; i++)
+        printf("%c ", inputText[i]);
     
     return 0;
 }
+void encoder(char *inputText, int key){
+    int n = 0;
+    while((inputText[n] >= 'A') && (inputText[n] <= 'z')){
+        inputText[n] = inputText[n] + key;
+        n++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //--------------------------------------------------------------------------//
 void rotate(char *letter){
     int n;
