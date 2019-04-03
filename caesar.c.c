5d6d089de3fbd7b5e3466e91279d193191795bb7
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-03 05:17:08 +0000

    Dakota Crypto
    Gotta blast, just tested assigning 'T' to oT and so on, sus

--- caesar.c.c
+++ caesar.c.c
@@ -7,7 +7,7 @@ void subDecoder(char *inputText, int *wordMap);
 
 int main(){   
     char inputText[100] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
-    int key = 1;        //hardcoded atm
+    int key = 3;        //hardcoded atm
     int i;
     int wordMap[10];
 
@@ -22,14 +22,14 @@ int main(){
         printf("%c ", inputText[i]);
         
     printf("\nDecoded: ");
-    decoder(inputText, key);
+    subDecoder(inputText, wordMap);
     for (i = 0; i <= 99; i++)
         printf("%c ", inputText[i]);
     
-    subDecoder(inputText, wordMap);
+    
     
     printf("\nWord Map: ");
-    for (i = 0; i <= 10; i++)
+    for (i = 0; i <= 8; i++)        //change 8 to bigger number
         printf("%d ", wordMap[i]);
     
     return 0;
@@ -39,6 +39,7 @@ int main(){
 
 void subDecoder(char *inputText, int *wordMap){
     int c = 0, n;
+    char oT, oH, oE;
     
  
 
@@ -51,8 +52,29 @@ void subDecoder(char *inputText, int *wordMap){
             c++;
         }
     }
+    // for each time 3 was mapped, turn that word into 'the' (only map first 'the' then assign 'the' to other 3 letter words)
+    for(n = 0; n < 20; n+=2){
+        if(wordMap[n] == 3){
+            oT = inputText[n];
+            inputText[n] = 'T';
+            oH = inputText[n + 1];
+            inputText[n + 1] = 'H';
+            oE = inputText[n + 2];
+            inputText[n + 2] = 'E';
+            break;
+        }
+    }
+    // assign T H and E (THESE WORDS COULD ALSO BE AND SO LATER WILL CHECK THAT E IS THE MOST COMMON) oT
+    for(n = 0; n < 100; n++){
+        if(inputText[n] == oT){
+            inputText[n] = 'T';
+        }else if(inputText[n] == oH){
+            inputText[n] = 'H';
+        }else if(inputText[n] == oE){           //First test and fox and dog did not change to 'the', sus
+            inputText[n] = 'E';
+    }
 }
-
+}
     
     
     
