cf239c45813810af2840595b205ec61f2e9228f5
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-03 03:39:26 +0000

    Caesar cipher
    Working on mapping how many letter are in each word for subDecoder

--- caesar.c.c
+++ caesar.c.c
@@ -1,30 +1,72 @@
+//-------------------------Dakota-----------------------------//
 #include <stdio.h>
 #include <stdlib.h>
 //void rotate(char *letter, int key);
 void encoder(char *inputText, int key);
 void decoder(char *inputText, int key);
+void subDecoder(char *inputText, char *wordMap);
 
 int main(){   
-    char inputText[100] = "HELLO THERE";
-    int key = 3;        //hardcoded atm
+    char inputText[100] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+    int key = 1;        //hardcoded atm
     //char letter[50]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     int i;
-        
+    char wordMap[20];
+
             
-    /*rotate(letter, key);       //code to test rotation for decoding
-    for (i = 0; i <= 26; i++)
-        printf("%c ", letter[i]); */
-    
+    printf("Input  : ");
+    for (i = 0; i <= 99; i++)
+        printf("%c ", inputText[i]);
+        
+    printf("\nEncoded: ");
     encoder(inputText, key);
-    for (i = 0; i <= 26; i++)
+    for (i = 0; i <= 99; i++)
+        printf("%c ", inputText[i]);
+        
+    printf("\nDecoded: ");
+    decoder(inputText, key);
+    for (i = 0; i <= 99; i++)
         printf("%c ", inputText[i]);
     
-    decoder(inputText, key);
-    for (i = 0; i <= 26; i++)
-        printf("%c ", inputText[i]);
+    printf("\nWord Map: ");
+    for (i = 0; i <= 99; i++)
+        printf("%c ", wordMap[i]);
     
     return 0;
 }
+
+//-----------------------------------------------------------------------------------------------------------//
+
+void subDecoder(char *inputText, char *wordMap){
+    int c = 0, n;
+    
+    for(c = 0; c < 100; c++){
+        wordMap[c] = 0;   
+    }
+    
+    c = 0;
+    
+    for(n = 0; n < 100; n++){
+        if(inputText[n] == ' '){
+            c++;
+        }else if((inputText[n] >= 'A') && (inputText[n] <= 'Z')){
+            wordMap[c] = wordMap[c] + 1;
+        }
+    
+}
+
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
 
 //--------------------------------------------------------------------------//
 
@@ -32,11 +74,13 @@ void encoder(char *inputText, int key){
     int n = 0;
     while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ')){
         if(inputText[n] == ' '){
-            inputText[n] = inputText[n] + 1 + key - 26;
+            inputText[n] = inputText[n] - 2 + key - 26;
         }
-        inputText[n] = inputText[n] - 1 - key;
+        inputText[n] = inputText[n] + 2 - key;
         if(inputText[n] < 'A'){
            inputText[n] = inputText[n] + 26;
+        }else if(inputText[n] > 'Z'){
+            inputText[n] = inputText[n] - 26;
         }
         n++;
     }   
@@ -48,9 +92,9 @@ void decoder(char *inputText, int key){
     int n = 0;
     while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ')){ 
         if(inputText[n] == ' '){
-            inputText[n] = inputText[n] - 1 - key - 26;
+            inputText[n] = inputText[n] + 2 - key - 26;
         }
-        inputText[n] = inputText[n] + 1 + key;
+        inputText[n] = inputText[n] - 2 + key;
         if(inputText[n] < 'A'){
             inputText[n] = inputText[n] + 26;
         }else if(inputText[n] > 'Z'){
@@ -74,8 +118,5 @@ void rotate(char *letter, int key){
 }
 }
 */
-
-
-
 
 
