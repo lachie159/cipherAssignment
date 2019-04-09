3ff792f0e117d7b742e6f45af3b6aab68d6e13b6
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-09 02:53:57 +0000

    working on rotDecoder (without key)

--- caesar.c.c
+++ caesar.c.c
@@ -1,15 +1,36 @@
 //-------------------------Dakota-----------------------------//
 #include <stdio.h>
 #include <stdlib.h>
-void encoder(char *inputText, int key);
-void decoder(char *inputText, int key);
+void keyEncoder(char *inputText, int key);
+void keyDecoder(char *inputText, int key);
+void rotDecoder(char *inputText);
 void subDecoder(char *inputText, int *wordMap);
 
-int main(){   
-    char inputText[100] = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+int main(){
+    char inputText[100];
     int key = 3;        //hardcoded atm
     int i;
-    int wordMap[10];
+    //int wordMap[10];
+    
+    FILE *input/*, output*/;
+    input = fopen("input.txt", "r");
+        if(input == NULL){
+            perror("fopen()");
+            return 0;
+        }
+        
+    for(i = 0; i < 100; i++){       //resets all string values to 0
+        inputText[i] = 0;
+    }
+
+    for(i = 0; feof(input) == 0; i++){          //scans file to inputText string
+        char *c;
+        c = &inputText[i];
+        printf("%d ", i);
+        fscanf(input, "%c", c);
+        
+    }
+        printf("\n");
 
             
     printf("Input  : ");
@@ -17,33 +38,47 @@ int main(){
         printf("%c ", inputText[i]);
         
     printf("\nEncoded: ");
-    encoder(inputText, key);
+    keyEncoder(inputText, key);
     for (i = 0; i <= 99; i++)
         printf("%c ", inputText[i]);
         
-    printf("\nDecoded: ");
-    subDecoder(inputText, wordMap);
-    for (i = 0; i <= 99; i++)
-        printf("%c ", inputText[i]);
+    for(key = 0; key = 25; key++){
+        printf("\nDecoded: ");
+        keyDecoder(inputText, key);
+        for (i = 0; i <= 99; i++)
+            printf("%c ", inputText[i]);
+    }
     
     
-    
-    printf("\nWord Map: ");
-    for (i = 0; i <= 8; i++)        //change 8 to bigger number
-        printf("%d ", wordMap[i]);
+    /*printf("\nWord Map: ");
+    for (i = 0; i <= 8; i++)        //change 8 to bigger number, 8 atm to stop '0 0 0 0...'
+        printf("%d ", wordMap[i]); */
     
     return 0;
 }
 
-//-----------------------------------------------------------------------------------------------------------//
+//---------------------------------------------rotDecoder-----------------------------------------------------//
+/*void rotDecoder(char *inputText){       //similiar to keyDecoder but will run until all the words are real, option to say no this isnt it
+    int n = 0;
+    int key = 3;
+    
+    
+    
+        }
 
+    }
+}
+
+*/
+
+
+//---------------------------------------------subDecoder------------------------------------------------------//
 void subDecoder(char *inputText, int *wordMap){
     int c = 0, n;
-    char oT, oH, oE;
     
  
 
-    for(n = 0; n < 100; n++){
+    for(n = 0; n < 100; n++){       //finds out how long each word is
         if(inputText[n] < ' '){
             break;
         }else if(inputText[n] != ' '){
@@ -52,7 +87,7 @@ void subDecoder(char *inputText, int *wordMap){
             c++;
         }
     }
-    // for each time 3 was mapped, turn that word into 'the' (only map first 'the' then assign 'the' to other 3 letter words)
+    /* for each time 3 was mapped, turn that word into 'the' (only map first 'the' then assign 'the' to other 3 letter words)
     for(n = 0; n < 20; n+=2){
         if(wordMap[n] == 3){
             oT = inputText[n];
@@ -63,8 +98,8 @@ void subDecoder(char *inputText, int *wordMap){
             inputText[n + 2] = 'E';
             break;
         }
-    }
-    // assign T H and E (THESE WORDS COULD ALSO BE AND SO LATER WILL CHECK THAT E IS THE MOST COMMON) oT
+    }*/
+    /* assign T H and E (THESE WORDS COULD ALSO BE AND SO LATER WILL CHECK THAT E IS THE MOST COMMON) oT
     for(n = 0; n < 100; n++){
         if(inputText[n] == oT){
             inputText[n] = 'T';
@@ -73,8 +108,8 @@ void subDecoder(char *inputText, int *wordMap){
         }else if(inputText[n] == oE){           //First test and fox and dog did not change to 'the', sus
             inputText[n] = 'E';
     }
+    }*/
 }
-}
     
     
     
@@ -82,9 +117,9 @@ void subDecoder(char *inputText, int *wordMap){
 
 
 
-//--------------------------------------------------------------------------//
+//-----------------------------keyEncoder------------------------------------//
 
-void encoder(char *inputText, int key){
+void keyEncoder(char *inputText, int key){
     int n = 0;
     while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ')){
         if(inputText[n] == ' '){
@@ -100,9 +135,9 @@ void encoder(char *inputText, int key){
     }   
 }
 
-//--------------------------------------------------------------------------//
+//-----------------------------keyDecoder-----------------------------------//
 
-void decoder(char *inputText, int key){
+void keyDecoder(char *inputText, int key){
     int n = 0;
     while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ')){ 
         if(inputText[n] == ' '){
