932911b9ea4498dac58cbd0be765bcb51ae38fcd
Lachlan Cullum <c3330990@uon.edu.au>
2019-04-28 23:30:39 +0000

    That'll have to do, im going insane

--- caesar.c.c
+++ caesar.c.c
@@ -1,9 +1,10 @@
 //-------------------------Dakota-----------------------------//
 #include <stdio.h>
 #include <stdlib.h>
-void keyEncoder(char *inputText, int key);
-void keyDecoder(char *inputText, int key);
-void keyFinder(char *inputText, int limit, int *keyf);
+void keyEncoder(char *inputText, int key); // Encodes plain text
+void keyDecoder(char *inputText, int key); // Decodes encrypted text with a key
+void allDecoder(char *inputText, int key); // Decodes encrypted text when key cant be found
+void keyFinder(char *inputText, int limit, int *keyf); // Finds the key of an encrypted message
 
 
 int main(){
@@ -16,8 +17,9 @@ int main(){
     int answer;
     printf("          Welcome to the Dakota Encoder/Decoder\n\nMy name is not Dakota but I found the wordplay slightly amusing\n\n");
     printf("1) Encrypt with key \n2) Decrypt with key \n3) Decrypt without key (only works for sentences containing 'the')\n");
-    //scanf("%d", &answer);
-    answer = 1; //-------------------------------------------------------------------------
+    answer = 1;
+    scanf("%d", &answer);
+     //-------------------------------------------------------------------------
     if(answer > 3 || answer < 1)
         return 0;
     
@@ -48,45 +50,76 @@ int main(){
         limit = i;
     }
     
+    for(i = 0; i < limit; i++){             //converts lowercase letters to uppercase
+        if((inputText[i] >= 'a') && (inputText[i] <= 'z')){
+            inputText[i] = inputText[i] - 32;}
+    }    
+    
     
     switch(answer){
-        case 1: key = (rand() % 27 + 1);
+        case 1: if(key == 0){
+                    key = 3;
+                }
                 printf("\nKey    : %d", key);
                 keyEncoder(inputText, key);
                 printf("\nEncoded: ");
                 
-                for (i = 0; i <= 99; i++)
+                for (i = 0; i <= 999; i++)
                     printf("%c ", inputText[i]);
-                break;
+                printf("\n");
+                return 0;
                 
         case 2: printf("\nInput  : ");
-                for (i = 0; i <= 99; i++)
+                for (i = 0; i <= limit; i++)
                     printf("%c ", inputText[i]);
                 printf("\n");
                 printf("Key    : %d", key);
                 keyDecoder(inputText, key);
                 printf("\nDecoded: ");
                 
-                for (i = 0; i <= 99; i++)
+                for (i = 0; i <= limit; i++)
                     printf("%c ", inputText[i]);
-                break;
+                printf("\n");
+                return 0;
                 
         case 3: printf("\nInput  : ");
-                for (i = 0; i <= 99; i++)
+                for (i = 0; i <= limit; i++)
                     printf("%c ", inputText[i]);
                 printf("\n");
                 
                 keyFinder(inputText, limit, keyf);
-                if(key < 0)
+                if(key < 0){
                     key += 26;
-                printf("Key    : %d", key);
-                
-                keyDecoder(inputText, key);
+                }
+                if(key != 0){
+                    printf("Key    : %d", key);
+                    keyDecoder(inputText, key);
                 printf("\nDecoded: ");
                 
-                for (i = 0; i <= 99; i++)
+                for (i = 0; i < limit; i++)
                     printf("%c ", inputText[i]);
-                break;
+                printf("\n");
+                return 0;
+                }else{
+                    for(key = 1; key < 26; key++){
+                        allDecoder(inputText, key);
+                        
+                        printf("\n");
+                        
+                        
+                        
+                        printf("\nDecoded: ");
+                        for (i = 0; i < limit; i++)
+                            printf("%c ", inputText[i]);
+                            printf("\n");
+                            
+                        keyDecoder(inputText, key);
+                        printf("\nDecoded: ");
+                        for (i = 0; i < limit; i++)
+                            printf("%c ", inputText[i]); 
+                    }
+                } 
+                return 0;
     }
 }
 
@@ -110,8 +143,8 @@ void keyFinder(char *inputText, int limit, int *keyf){
 
 void keyEncoder(char *inputText, int key){
     int n = 0;
-    while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ')){
-        if(inputText[n] == ' '){
+    while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ') || (inputText[n] == ':') || (inputText[n] == ',') || (inputText[n] == '\'') || (inputText[n] == '.') || (inputText[n] == '-')){
+        if(inputText[n] > 'Z' || inputText[n] < 'A'){
             inputText[n] = inputText[n] + key - 26;
         }
         inputText[n] = inputText[n] - key;
@@ -124,11 +157,13 @@ void keyEncoder(char *inputText, int key){
     }   
 }
 
+
+
 //-----------------------------keyDecoder-----------------------------------//
 
 void keyDecoder(char *inputText, int key){
     int n = 0;
-    while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ') || (inputText[n] == ':') || (inputText[n] == ',') || (inputText[n] == 96) || (inputText[n] == '.') || (inputText[n] == '-') ){ 
+    while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ') || (inputText[n] == ':') || (inputText[n] == ',') || (inputText[n] == '\'') || (inputText[n] == '.') || (inputText[n] == '-')){ 
         if(inputText[n] > 'Z' || inputText[n] < 'A'){
             inputText[n] = inputText[n] - key - 26;
         }
@@ -141,3 +176,23 @@ void keyDecoder(char *inputText, int key){
         n++;
     }   
 }
+
+//-----------------------------allDecoder-----------------------------------//
+
+void allDecoder(char *inputText, int key){
+    int n = 0;
+    while(((inputText[n] >= 'A') && (inputText[n] <= 'Z')) || (inputText[n] == ' ') || (inputText[n] == ':') || (inputText[n] == ',') || (inputText[n] == '\'') || (inputText[n] == '.') || (inputText[n] == '-')){ 
+        if(inputText[n] > 'Z' || inputText[n] < 'A'){
+            inputText[n] = inputText[n] - key - 27;
+        }
+        inputText[n] = inputText[n] + key + 1;
+        if(inputText[n] < 'A'){
+            inputText[n] = inputText[n] + 26;
+        }else if(inputText[n] > 'Z'){
+            inputText[n] = inputText[n] - 26; 
+        }
+        n++;
+    }   
+    
+}
+
